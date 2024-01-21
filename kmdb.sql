974ef727de7898f8e2160bd18e9bb93b5a4192c7
Jodi Kraushar <jodi.kraushar@kellogg.northwestern.edu>
2024-01-21 23:54:48 +0000

    Drop and Create Tables for movie database assignment

--- kmdb.sql
+++ kmdb.sql
@@ -105,13 +105,95 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS roles;
 
 -- Create new tables, according to your domain model
 -- TODO!
 
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  year INTEGER
+  rating TEXT,
+  studio_id TEXT,
+  role_id TEXT
+);
+
+CREATE TABLE studios (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE actors (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT
+);
+
+CREATE TABLE roles (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  actor_id TEXT,
+  movie_id TEXT
+);
+
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
+
+INSERT INTO movies (
+name,
+year,
+rating,
+studio_id
+) VALUES (
+    "Batman Begins",
+    2005,
+    "PG-13",
+    "Warner Bros."
+)
+(
+name,
+year,
+rating,
+studio_id
+) VALUES (
+    "The Dark Knight",
+    2008,
+    "PG-13",
+    "Warner Bros."
+)
+(
+name,
+year,
+rating,
+studio_id
+) VALUES (
+    "The Dark Knight Rises",
+    2012,
+    "PG-13",
+    "Warner Bros."
+)
+
+INSERT INTO movies (
+name,
+year,
+rating,
+studio_id
+) VALUES (
+    "Batman Begins",
+    2005,
+    "PG-13",
+    "Warner Bros."
+)
+
+Batman Begins          Christian Bale        Bruce Wayne
+-- Batman Begins          Michael Caine         Alfred
+-- Batman Begins          Liam Neeson           Ra's Al Ghul
+-- Batman Begins          Katie Holmes          Rachel Dawes
+-- Batman Begins          Gary Oldman           Commissioner Gordon
 
 -- Prints a header for the movies output
 .print "Movies"
